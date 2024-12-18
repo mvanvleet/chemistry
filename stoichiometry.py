afbab37d2997fc564efb50755e49549c5e05d294
Mary Van Vleet <mary.van.vleet@gmail.com>
2024-12-18 11:59:13 -0500

    Added flexibility to stoichiometry module to allow different ways of reading in MW data

--- stoichiometry.py
+++ stoichiometry.py
@@ -11,12 +11,19 @@ from . import elementdata
 
 # Function to obtain molecular weight of a compound from a .xyz file
 def MolecularWeight(coordinates):
-    """Given an input list of coordinates of the following form:
-    [[symbol1,x1,y1,z1],[symbol2,x2,y2,z2],...[symboln,xn,yn,zn]]
+    """Given an input list of coordinates of one of the the following forms:
+    A. list of lists: [[symbol1,x1,y1,z1],[symbol2,x2,y2,z2],...[symboln,xn,yn,zn]]
+    B. list: [symbol1, symbol2, symbol3]
     returns the molecular weight (in g/mol) of
     the compound specified by the list.
     """
-    elements=[atom[0] for atom in coordinates]
+
+    if type(coordinates[0]) == list:
+        elements=[atom[0] for atom in coordinates]
+    elif type(coordinates[0]) == str:
+        elements = coordinates
+    else:
+        raise TypeError('Input must be a list or a list of lists')
 
     # Calculate molecular weight
     molecular_weight=0.00
@@ -38,3 +45,5 @@ if __name__=='__main__':
     pwd = os.path.dirname(os.path.abspath(__file__))
     coordinates,title=io.ReadCoordinates('/'.join([pwd,'tests','test.xyz']))
     print(MolecularWeight(coordinates))
+    atoms=['C','H','H','H']
+    print(MolecularWeight(atoms))
